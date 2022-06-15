13ea923957b37968f6eb873033237c86150486d7
Jordi Irawan <jordirwn@gmail.com>
2022-06-15 14:52:06 +0700

    update style

--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,129 @@
-function handleMobileButton() {
-  const elBtn = document.getElementById('mobile-button');
-  const closeBtn = elBtn.querySelector('#close-btn');
-  const showBtn = elBtn.querySelector('#show-btn');
-  const mobileNavbar = document.querySelector('#mobile-navbar');
+class Modal {
+  constructor(modalId, opts = {}) {
+    this.modalId = modalId;
+    this.modal = document.getElementById(this.modalId);
+    this.modalTitle = document.getElementById(this.modalId + '-title');
+    this.modalMsg = document.getElementById(this.modalId + '-message');
+    this.classOpenName = opts.open || 'modal-open';
+    this.closeBtnId = opts.close || this.modalId + '-close-btn';
+  }
 
-  elBtn.addEventListener('click', () => {
-    closeBtn.classList.toggle('hidden');
-    showBtn.classList.toggle('hidden');
-    mobileNavbar.classList.toggle('hidden');
-  });
+  open(title, msg) {
+    this.modalTitle.textContent = title;
+    this.modalMsg.textContent = msg;
+    !this.modal.classList.contains(this.classOpenName) &&
+      this.modal.classList.add(this.classOpenName);
+  }
+
+  close() {
+    this.modalTitle.textContent = '';
+    this.modalMsg.textContent = '';
+    this.modal.classList.remove(this.classOpenName);
+  }
+
+  activeCloseBtn() {
+    document.getElementById(this.closeBtnId).addEventListener('click', (ev) => {
+      this.close();
+    });
+  }
 }
 
 function changeBgHeader(ev) {
   const header = document.querySelector('header');
+  const clsName = 'shadow-md';
   if (scrollY > 80) {
-    header.classList.contains('bg-header') &&
-      header.classList.remove('bg-header');
-    !header.classList.contains('bg-header-on-scroll') &&
-      header.classList.add('bg-header-on-scroll');
+    !header.classList.contains(clsName) && header.classList.add(clsName);
   } else {
-    !header.classList.contains('bg-header') &&
-      header.classList.add('bg-header');
-    header.classList.contains('bg-header-on-scroll') &&
-      header.classList.remove('bg-header-on-scroll');
+    header.classList.contains(clsName) && header.classList.remove(clsName);
   }
 }
-function handleSubmit(ev) {
-  const scriptURL =
-    'https://script.google.com/macros/s/AKfycbwVfEQVfZd17wcB38vRMNdJLHl2Sw0JadRZr9akD7T0NkI6QyCHYtwnxwvLU0cVUA9edA/exec';
-  ev.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(this) })
-    .then((response) => console.log('Success!', response))
-    .catch((error) => console.error('Error!', error.message));
-  this.reset();
+
+function scrollspy(ev) {
+  const links = document.querySelectorAll('[data-scrollspy]');
+  const scrollPos = window.scrollY || document.documentElement.scrollTop;
+
+  links.forEach((link) => {
+    const elem = document.querySelector(link.getAttribute('href'));
+
+    elem.offsetTop <= scrollPos &&
+    elem.offsetTop + elem.clientHeight > scrollPos
+      ? link.classList.add('active')
+      : link.classList.remove('active');
+  });
 }
-function handleOnScroll(ev) {
+
+function handleScroll(ev) {
   changeBgHeader(ev);
+  scrollspy(ev);
 }
 
-addEventListener('scroll', handleOnScroll);
-(function main() {
-  handleMobileButton();
-})();
+async function handleSubmit(ev) {
+  ev.preventDefault();
 
-document.forms[0].addEventListener('submit', handleSubmit);
+  const scriptURL =
+    'https://script.google.com/macros/s/AKfycbzz7UG-z0CFxYaCj-Ngh255G9uFZQG4t3LSysnS00tL4YEsRmXd0LS3El80gDThSCDmHg/exec';
+  const body = new FormData(this);
+  const submitBtn = this.querySelector('[type=submit]');
+  const modalForm = new Modal('form-modal');
+
+  modalForm.activeCloseBtn();
+
+  try {
+    submitBtn.classList.add('loading');
+    submitBtn.disabled = true;
+
+    const res = await fetch(scriptURL, { method: 'POST', body });
+    if ((await res.status) === 200) {
+      this.reset();
+      modalForm.open(
+        'Successfully',
+        `Thank you ${body.get('name')} for sending me a message`
+      );
+    } else {
+      modalForm.open(
+        'Ups, problem occurs!',
+        'Try sending again or check your data again'
+      );
+    }
+  } catch (e) {
+    modalForm.open('Ups, problem occurs!', 'Failed to send');
+  }
+
+  submitBtn.classList.remove('loading');
+  submitBtn.disabled = false;
+}
+
+function swapTheme() {
+  document
+    .getElementById('swap-theme')
+    .addEventListener('click', function (ev) {
+      this.classList.toggle('swap-active');
+      const theme = this.classList.contains('swap-active')
+        ? 'night'
+        : 'cupcake';
+      document.documentElement.dataset.theme = theme;
+      localStorage.theme = theme === 'cupcake' ? 'light' : 'dark';
+    });
+}
+
+function themeCheck() {
+  if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.dataset.theme = 'night';
+    document.getElementById('swap-theme').classList.toggle('swap-active');
+  } else {
+    document.documentElement.dataset.theme = 'cupcake';
+  }
+}
+
+function main() {
+  addEventListener('scroll', handleScroll);
+  swapTheme();
+  themeCheck();
+  document.forms['form-report'].addEventListener('submit', handleSubmit);
+}
+
+main();
