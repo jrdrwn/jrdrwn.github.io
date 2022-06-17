f323d2f94baaec922094c8e9a9da33cbf0b347ba
Jordi Irawan <jordirwn@gmail.com>
2022-06-17 07:02:23 +0700

    update content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['src/**/*.html'],
+  content: ['index.html', 'public/js/main.js'],
   theme: {
     extend: {
       container: {
